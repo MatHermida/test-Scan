6332cbde101d1d1dbbc4533a0a2b90ae155c0bb8
MatHermida <hermidamatias2@gmail.com>
2024-06-19 10:04:06 -0300

    fix width

--- src/pages/Explorer/Explorer.tsx
+++ src/pages/Explorer/Explorer.tsx
@@ -158,6 +158,7 @@ const Explorer = () => {
         <S.TVLChartTitle item>Total Value Locked Chart</S.TVLChartTitle>
         <S.TVLChart>
           <iframe
+            width="100%"
             height="360px"
             src="https://defillama.com/chart/protocol/c3-exchange?tvl=true&denomination=USD&theme=dark"
             title="C3"
