2aaa44327533ab1d5fba0606ea68f25cd77f3da7
MatHermida <hermidamatias2@gmail.com>
2024-08-23 17:34:08 -0300

    dissappear

--- src/pages/Explorer/components/TVLChart/TVLChart.tsx
+++ src/pages/Explorer/components/TVLChart/TVLChart.tsx
@@ -1,6 +1,60 @@
+import { useEffect, useRef, useState } from 'react';
 import * as S from './styles';
 
 const TVLChart = () => {
+  const [shouldShowIframe, setShouldShowIframe] = useState(true);
+  const [iframeLoaded, setIframeLoaded] = useState(false);
+  const iframeRef = useRef(null);
+
+  useEffect(() => {
+    const iframe: any = iframeRef.current;
+    console.log('iframe', iframe);
+
+    try {
+      const aux: any = iframe;
+      var innerDoc = aux?.contentWindow.document
+        ? aux?.contentWindow.document
+        : aux?.contentDocument;
+      const canvasElements = innerDoc.getElementsByTagName('canvas');
+
+      if (!canvasElements.length) {
+        // setShouldShowIframe(false);
+        console.log('no canvas elements');
+      } else {
+        setShouldShowIframe(true);
+        console.log('canvas elements');
+      }
+    } catch (error) {
+      console.error("Error accessing iframe's content:", error);
+      // setShouldShowIframe(false);
+    }
+
+    // const checkForCanvasElement = () => {
+    //   try {
+    //     const aux: any = iframe;
+    //     var innerDoc = (aux.contentWindow.document) ? aux.contentWindow.document : aux.contentDocument ;
+    //     const canvasElements = innerDoc.getElementsByTagName('canvas');
+
+    //     if (!canvasElements.length) {
+    //       setShouldShowIframe(false);
+    //     } else {
+    //       setShouldShowIframe(true);
+    //     }
+    //   } catch (error) {
+    //     console.error("Error accessing iframe's content:", error);
+    //     setShouldShowIframe(false);
+    //   }
+    // };
+    // iframe?.addEventListener('load', checkForCanvasElement);
+    // return () => {
+    //   iframe?.removeEventListener('load', checkForCanvasElement);
+    // };
+  }, [iframeLoaded]);
+
+  if (!shouldShowIframe) {
+    return <></>;
+  }
+
   return (
     <S.TVLChartContainer item mobile={12}>
       <S.TVLChartTitle item>
@@ -15,11 +69,17 @@ const TVLChart = () => {
       </S.TVLChartTitle>
       <S.TVLChart>
         <iframe
+          ref={iframeRef}
+          id="my-iframe"
           width="100%"
           height="360px"
-          src="https://defillama.com/chart/protocol/c3-exchange?tvl=true&volume=false&bridgeVolume=false&twitter=false&devMetrics=false&devCommits=false&groupBy=daily&theme=dark"
+          src="https://defillama.com/chart/protocol/c3-exchange?tvl=true&denomination=USD&theme=dark"
           title="C3"
           frameBorder="0"
+          onLoad={() => {
+            console.log('iframe loaded');
+            setIframeLoaded(true);
+          }}
         ></iframe>
       </S.TVLChart>
     </S.TVLChartContainer>
